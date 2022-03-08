6a5bb2ddfc4d419fad0af053a89a91f3f2599570
Jayden Pak <jppak86@gmail.com>
2022-03-07 21:05:33 -0500

    new team page

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,25 +1,69 @@
 import { Team } from '../models/team.js'
 import { Player } from '../models/player.js'
 
-function newTeam(req, res) {
-  for (let key in req.body) {
-    if(req.body[key] === "") delete req.body[key]
-  }
-  Team.findById(req.params.id)
-  .populate('GK RFB LFB CB Sweeper LMF RMF DMF AMF Central Striker', 'name')
-  .exec(function(err, team){
-    console.log(Team)
-    Player.find({_id: {$nin: [team.GK, team.RFB, team.LFB, team.CB, team.Sweeper, team.LMF, team.RMF, team.DMF, team.AMF, team.Central, team.Striker]}},
-      function(err, players){
-        res.render('teams/new', {
-          title: "Add Team",
-          players,
-          team
-        })
-    })
+async function newTeam (req, res) {
+  const teams = await Team.find({})
+  const players = await Player.find({})
+  res.render('teams/new', {
+    title: "Add Team",
+    teams,
+    players
   })
-  }
+}
+
+function create (req, res) {
+  Team.create(req.body).then(team => {
+    res.redirect("/")
+  })
+}
+
+// function newTeam(req, res) {
+//   Team.find({}).then(teams => {
+//     Player.find({}).then(players => {
+//       res.render('teams/new', {
+//             title: "Add Team",
+//             teams,
+//             players
+//           })
+//     })
+//   })
+
+  // Team.find({}, function (err, teams) {
+  //   res.render('teams/new', {
+  //     title: "Add Team",
+  //     teams
+  //   })
+  // })
+  // Player.find({}, function(error, players){
+  //   res.render('/new', {
+  //     title: 'Add Player',
+  //     players,
+  //   })
+  // })
+
+// }
+
+
+// function newTeam(req, res) {
+//   for (let key in req.body) {
+//     if(req.body[key] === "") delete req.body[key]
+//   }
+//   Team.findById(req.params.id)
+//   .populate({ path: 'GK RFB LFB CB Sweeper LMF RMF DMF AMF Central Striker', 'name')
+//   .exec(function(err, team){
+//     console.log(Team)
+//     Player.find({_id: {$nin: [team.GK, team.RFB, team.LFB, team.CB, team.Sweeper, team.LMF, team.RMF, team.DMF, team.AMF, team.Central, team.Striker]}},
+//       function(err, players){
+//         res.render('teams/new', {
+//           title: "Add Team",
+//           players,
+//           team
+//         })
+//     })
+//   })
+//   }
 
 export {
   newTeam as new,
+  create
 }
--- routes/teams.js
+++ routes/teams.js
@@ -5,6 +5,7 @@ import * as teamCtrl from '../controllers/teams.js'
 const router = Router()
 
 router.get('/new', teamCtrl.new)
+router.post('/', teamCtrl.create)
 
 export {
   router
