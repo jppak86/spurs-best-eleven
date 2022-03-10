07fa3ee3695fd6d73cba3eb73fc9bb1d6efb1cd4
Jayden Pak <jppak86@gmail.com>
2022-03-10 11:17:43 -0500

    index page update

--- routes/players.js
+++ routes/players.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as playerCtrl from '../controllers/players.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 
 const router = Router()
 
 router.get('/new', playerCtrl.new)
-router.post('/', playerCtrl.create)
-router.delete("/:id", playerCtrl.delete)
+router.post('/', isLoggedIn, playerCtrl.create)
+router.delete("/:id", isLoggedIn, playerCtrl.delete)
 
 export {
   router
--- routes/teams.js
+++ routes/teams.js
@@ -6,12 +6,12 @@ import * as teamCtrl from '../controllers/teams.js'
 const router = Router()
 
 router.get('/new', teamCtrl.new)
-router.post('/', teamCtrl.create)
+router.post('/', isLoggedIn, teamCtrl.create)
 router.get("/", teamCtrl.index)
 router.get("/:id", teamCtrl.show)
-router.delete("/:id", teamCtrl.delete)
-router.get("/:id/edit", teamCtrl.edit)
-router.put("/:id", teamCtrl.update)
+router.delete("/:id", isLoggedIn, teamCtrl.delete)
+router.get("/:id/edit", isLoggedIn, teamCtrl.edit)
+router.put("/:id", isLoggedIn, teamCtrl.update)
 
 router.post('/', isLoggedIn, teamCtrl.create)
 
